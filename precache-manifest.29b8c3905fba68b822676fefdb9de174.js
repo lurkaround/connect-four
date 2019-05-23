50554775a880e6651cb81bb6883956fb4b132375
Helon <rhelon@gmail.com>
2019-05-23 11:48:57 +0200

    Updates

---
+++ precache-manifest.29b8c3905fba68b822676fefdb9de174.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af94ae31570a7600e93eaa68f7d6a3c2",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "b93fcc402d8ea518631c",
+    "url": "/connect-four/static/css/main.857ff250.chunk.css"
+  },
+  {
+    "revision": "e3e1726d43da044c5a6a",
+    "url": "/connect-four/static/js/2.7cddf10a.chunk.js"
+  },
+  {
+    "revision": "b93fcc402d8ea518631c",
+    "url": "/connect-four/static/js/main.bcfeec34.chunk.js"
+  },
+  {
+    "revision": "5f78906a733342f2d383",
+    "url": "/connect-four/static/js/runtime~main.000e00dd.js"
+  }
+]);
